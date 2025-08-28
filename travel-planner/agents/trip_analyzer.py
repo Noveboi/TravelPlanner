b0b8140288cee1cef3652c3f70b9fea1d4a71fd0
George Nikolaidis <nove@novesoftware.net>
2025-08-29 00:40:25 +0300

    Add `invoke` method in `TripAnalyzerAgent` and add logging

--- travel-planner/agents/trip_analyzer.py
+++ travel-planner/agents/trip_analyzer.py
@@ -1,16 +1,33 @@
-from datetime import date
-from typing import List
+from typing import cast
+import logging
 
-from langchain_core.language_models import BaseLanguageModel
-from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage
-from models.trip import TripRequest, TravelStyle
+from langchain_core.runnables import Runnable
+from langchain_core.language_models import BaseLanguageModel, LanguageModelInput
+from langchain_core.messages import SystemMessage, HumanMessage
+from models.trip import TripProfile, TripRequest
 
 class TripAnalyzerAgent:
     def __init__(self, llm: BaseLanguageModel) -> None:
-        self._llm = llm
+        self._logger = logging.getLogger(name='trip_analyzer')
+
+        self._logger.info('Initializing')
+        structured_llm = llm.with_structured_output(schema=TripProfile)
+
+        # We do a cast here because `with_structured_output` does not return a proper type assignable to the desired.
+        self._llm: Runnable[LanguageModelInput, TripProfile] = cast(
+            Runnable[LanguageModelInput, TripProfile],
+            structured_llm
+        )
+
+        self._logger.info('Initialized')
+
+    def invoke(self, request: TripRequest) -> TripProfile:
+        self._logger.info('Invoked')
+        prompt = self._create_analysis_prompt(request)
+        return self._llm.invoke(prompt)
 
 
-    def _create_analysis_prompt(self, req: TripRequest) -> list[BaseMessage]:
+    def _create_analysis_prompt(self, req: TripRequest) -> LanguageModelInput:
         duration = req.duration
         daily_budget = req.budget / duration if duration > 0 else req.budget
         
@@ -24,6 +41,9 @@ class TripAnalyzerAgent:
             5. Recommendations: Specific suggestions to enhance their travel experience
             
             Provide concise, actionable insights that will help personalize their trip.
+                          
+            Information about the structured output:
+            - The fields ending with 'score' are numbers in the range from 0 to 1.
             """),
             HumanMessage(content=f"""
             Analyze this trip request:
