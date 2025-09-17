c5fe7419fb9293b47685e864d65db932c62bfcba
George Nikolaidis <nove@novesoftware.net>
2025-09-17 11:51:13 +0300

    Create `DestinationScout` class

---
+++ travel-planner/agents/destination_scout.py
@@ -0,0 +1,9 @@
+﻿from langchain_core.language_models import BaseLanguageModel
+
+from destination import DestinationReport
+
+from core import BaseAgent
+
+class DestinationScoutAgent(BaseAgent):
+    def __init__(self, llm: BaseLanguageModel) -> None:
+        super().__init__('destination_scout', llm.with_structured_output(schema=DestinationReport))
