a2ada346407458cac65f4bc44c1354d4885c2085
George Nikolaidis <nove@novesoftware.net>
2025-09-17 10:49:35 +0300

    Refactor: `TripProfile` to `GeneralTripAnalysis`

--- travel-planner/agents/trip_analyzer.py
+++ travel-planner/agents/trip_analyzer.py
@@ -5,7 +5,7 @@ from langchain_core.language_models import BaseLanguageModel, LanguageModelInput
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.runnables import Runnable
 
-from .trip import TripProfile, TripRequest
+from .trip import GeneralTripAnalysis, TripRequest
 
 
 class TripAnalyzerAgent:
@@ -13,25 +13,25 @@ class TripAnalyzerAgent:
         self._logger = logging.getLogger(name='trip_analyzer')
 
         self._logger.info('Initializing')
-        structured_llm = llm.with_structured_output(schema=TripProfile)
+        structured_llm = llm.with_structured_output(schema=GeneralTripAnalysis)
 
         # We do a cast here because `with_structured_output` does not return a proper type assignable to the desired.
-        self._llm: Runnable[LanguageModelInput, TripProfile] = cast(
-            Runnable[LanguageModelInput, TripProfile],
+        self._llm: Runnable[LanguageModelInput, GeneralTripAnalysis] = cast(
+            Runnable[LanguageModelInput, GeneralTripAnalysis],
             structured_llm
         )
 
         self._logger.info('Initialized')
 
-    def invoke(self, request: TripRequest) -> TripProfile:
+    def invoke(self, request: TripRequest) -> GeneralTripAnalysis:
         self._logger.info('Invoked')
         prompt = self._create_analysis_prompt(request)
         return self._llm.invoke(prompt)
 
 
-    def _create_analysis_prompt(self, req: TripRequest) -> LanguageModelInput:
+    @staticmethod
+    def _create_analysis_prompt(req: TripRequest) -> LanguageModelInput:
         duration = req.duration
-        daily_budget = req.budget / duration if duration > 0 else req.budget
         
         return [
             SystemMessage(content="""
@@ -45,25 +45,19 @@ class TripAnalyzerAgent:
             Provide concise, actionable insights that will help personalize their trip.
                           
             Information about the structured output:
-            - The "preferred_activities" field is a list of general activities based on the trip request's data. Examples
-            of preferred activities include: Sightseeing, Museums, Concerts, Leisure, Workshops, Hiking, Luxury Dining, Cozy Dining, etc...
             - The "group_focus" field is a list of keywords that describe what the group should focus on based on the type of group.
-            For example, a "Couple" group type could focus on Romance and Atmosphere, while a "Family" group type could focus a Fun for all ages and Safety
             - The "group_recommendations" field is a list of activities/places that the group can do/go to. 
-            - The "time_allocation" field allocates a weight to each activity found you've added in "preferred_activities" and "group_recommendations".
-            The bigger the weight, the more time should be allocated to that activity.
             - The "key_recommendations" field contains your best/top recommendations to enhance the trip experience.
-            - The "potential_challenges" field contains issues or problems that might arise that the user needs to be made aware of.
-            Potential challenges can be budget limitations or location-specific issues.
+            - The "potential_challenges" field contains issues or problems that might arise that the user needs to be made aware of. Potential challenges can be budget limitations or location-specific issues.
             """),
             HumanMessage(content=f"""
             Analyze this trip request:
-            Destination: {req.destination}
-            Duration: {duration} days ({req.start_date} to {req.end_date})
-            Budget: ${req.budget:,.2f} EUR (approximately ${daily_budget:,.2f} per day)
-            Group: {req.travelers} travelers - {req.trip_type.value.title()} trip
-            Travel Style(s): {req.format_travel_styles()}
-            Interests: {req.format_interests()}
+            - Destination: {req.destination}
+            - Duration: {duration} days ({req.start_date} to {req.end_date})
+            - Budget: ${req.budget:,.2f} EUR
+            - Group: {req.travelers} travelers - '{req.trip_type.value.title()}' trip
+            - Travel Style(s): {req.format_travel_styles()}
+            - Interests: {req.format_interests()}
             
             Please provide a comprehensive analysis based on the above criteria.
             """)]
