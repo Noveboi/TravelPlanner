6da6e4594cb43a2260c6a6d12441da2539e7383d
George Nikolaidis <nove@novesoftware.net>
2025-09-17 11:50:05 +0300

    Create `BaseAgent` class in `core.py`

---
+++ travel-planner/agents/core.py
@@ -0,0 +1,13 @@
+﻿from langchain_core.language_models import LanguageModelInput
+from langchain_core.runnables import Runnable
+from typing import TypeVar
+from pydantic import BaseModel
+import logging
+
+T = TypeVar('T', bound=BaseModel)
+
+class BaseAgent:
+    def __init__(self, name: str, llm: Runnable[LanguageModelInput, T]) -> None:
+        self._logger = logging.getLogger(name=name)
+        self._llm = llm
+        self._logger.info('✅ Initialized')
