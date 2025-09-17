a6ef51998d0bdf1f2b4f414b01ee46bdff672108
George Nikolaidis <nove@novesoftware.net>
2025-09-17 10:37:54 +0300

    Add main check in `main.py`

--- travel-planner/main.py
+++ travel-planner/main.py
@@ -4,27 +4,29 @@ from agents.trip_analyzer import TripAnalyzerAgent
 from agents.trip import TravelStyle, TripType, TripRequest
 import logging
 
-logging.basicConfig(level=logging.INFO) 
 
-log = logging.getLogger('main')
-
-log.info('Starting')
-llm = ChatOllama(model='llama3.1:latest')
-log.info('Got LLM model')
-
-agent = TripAnalyzerAgent(llm)
-
-request = TripRequest(
-    destination='London',
-    start_date=date(2025,9,16),
-    end_date=date(2025,9,21),
-    budget=2000,
-    travelers=2,
-    travel_styles=[TravelStyle.ADVENTURE],
-    trip_type=TripType.COUPLE,
-    interests=['Sightseeing', 'Food']
-)
-
-profile = agent.invoke(request)
-
-print(profile)
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO) 
+    
+    log = logging.getLogger('main')
+    
+    log.info('Starting')
+    llm = ChatOllama(model='llama3.1:latest')
+    log.info('Got LLM model')
+    
+    agent = TripAnalyzerAgent(llm)
+    
+    request = TripRequest(
+        destination='London',
+        start_date=date(2025,9,16),
+        end_date=date(2025,9,21),
+        budget=2000,
+        travelers=2,
+        travel_styles=[TravelStyle.ADVENTURE],
+        trip_type=TripType.COUPLE,
+        interests=['Sightseeing', 'Food']
+    )
+    
+    profile = agent.invoke(request)
+    
+    print(profile)
