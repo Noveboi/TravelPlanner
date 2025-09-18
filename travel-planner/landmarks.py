dee22d12718b4bf709b3b562457f8f057ef7c7bd
George Nikolaidis <nove@novesoftware.net>
2025-09-18 20:54:43 +0300

    Reconfigure "main" to be modular and easily testable

---
+++ travel-planner/landmarks.py
@@ -0,0 +1,9 @@
+﻿from agents.destination_scout import LandmarkScoutAgent
+from setup import llm_with_tools, example_request
+import json
+
+if __name__ == '__main__':
+    agent = LandmarkScoutAgent(llm_with_tools)
+    report = agent.invoke(example_request)
+
+    print(json.dumps(report.model_dump(), indent=2))
---
+++ travel-planner/setup.py
@@ -0,0 +1,26 @@
+﻿import logging
+import os
+from datetime import date
+from agents.trip import TravelStyle, TripType, TripRequest
+from tools.tools import bind_necessary_tools
+from langchain_ollama import ChatOllama
+
+os.environ['TAVILY_API_KEY'] = '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'
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger('main')
+
+log.info('Starting')
+llm = ChatOllama(model='llama3.1:latest')
+llm_with_tools = bind_necessary_tools(llm)
+log.info('Got LLM model')
+
+example_request = TripRequest(
+    destination='London',
+    start_date=date(2025, 9, 16),
+    end_date=date(2025, 9, 21),
+    budget=2000,
+    travelers=2,
+    travel_styles=[TravelStyle.ADVENTURE],
+    trip_type=TripType.COUPLE,
+    interests=['Sightseeing', 'Food']
+)
