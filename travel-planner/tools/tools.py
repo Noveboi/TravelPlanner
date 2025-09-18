fed5a167bfdd9784116cd9341d8df553fc8528ab
George Nikolaidis <nove@novesoftware.net>
2025-09-18 20:54:49 +0300

    Add `tools` package

---
+++ travel-planner/tools/tools.py
@@ -0,0 +1,12 @@
+﻿from typing import Any, Sequence
+
+from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import BaseMessage
+from langchain_core.prompt_values import PromptValue
+from langchain_core.runnables import Runnable
+from langchain_tavily import TavilySearch
+
+def bind_necessary_tools(llm: BaseChatModel) -> Runnable[PromptValue | str | Sequence[BaseMessage | list[str] | tuple[str, str] | str | dict[str, Any]], BaseMessage]:
+    return llm.bind_tools([
+        TavilySearch(max_results=10)
+    ])
