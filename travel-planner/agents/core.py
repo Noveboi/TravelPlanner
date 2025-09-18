cd0c98145c1473dca2ad6ad2fcb96a7a71e84716
George Nikolaidis <nove@novesoftware.net>
2025-09-18 20:55:31 +0300

    Add lifetime callbacks to LLM

--- travel-planner/agents/core.py
+++ travel-planner/agents/core.py
@@ -3,6 +3,7 @@ from typing import TypeVar
 
 from langchain_core.language_models import LanguageModelInput
 from langchain_core.runnables import Runnable
+from langsmith import RunTree
 from pydantic import BaseModel
 
 T = TypeVar('T', bound=BaseModel)
@@ -11,5 +12,20 @@ T = TypeVar('T', bound=BaseModel)
 class BaseAgent:
     def __init__(self, name: str, llm: Runnable[LanguageModelInput, T]) -> None:
         self._logger = logging.getLogger(name=name)
-        self._llm = llm
+        self._llm = llm.with_listeners(
+            on_start=self._start_listener,
+            on_end=self._end_listener,
+            on_error=self._error_listener
+        )
+        
         self._logger.info('✅ Initialized')
+        
+
+    def _start_listener(self, tree: RunTree) -> None:
+        self._logger.info(f"🤖 Agent Started at {tree.start_time}")
+    
+    def _end_listener(self, tree: RunTree) -> None:
+        self._logger.info(f"🤖 Agent Finished at {tree.end_time}")
+        
+    def _error_listener(self, tree: RunTree) -> None:
+        self._logger.error(f"🤖 Agent error: {tree.error}")
