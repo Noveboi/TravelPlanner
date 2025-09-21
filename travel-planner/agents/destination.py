06ea45684131ef0646f50ef0bdbadfa09a20a9ae
George Nikolaidis <nove@novesoftware.net>
2025-09-21 19:32:28 +0300

    Create `Event` models

--- travel-planner/agents/destination.py
+++ travel-planner/agents/destination.py
@@ -1,5 +1,5 @@
 ﻿from datetime import datetime
-from typing import List
+from typing import List, Optional
 from enum import Enum
 
 from pydantic import BaseModel, Field
@@ -11,12 +11,6 @@ class Priority(str, Enum):
     SHOULD_SEE = "Should See"
     NICE_TO_SEE = "Nice to See"
     CAN_SKIP = "Can Skip"
-
-class EstablishmentType(str, Enum):
-    RESTAURANT = "Restaurant"
-    CAFE = "Café"
-    BAR = "Bar"
-    PUB = "Pub"
 
 class Place(BaseModel):
     name: str = Field(
@@ -31,11 +25,22 @@ class Place(BaseModel):
     reason_to_go: str = Field(
         description="A short reason why one should go to this place"
     )
+    website: Optional[str] = Field(description="The website for the place. If there is one.")
 
 class Establishment(Place):
-    establishment_type: EstablishmentType
+    establishment_type: str = Field(
+        description="The type of the establishment",
+        examples=['Restaurant', 'Cafe', 'Bar', 'Pub', 'Tavern', 'Canteen']
+    )
 
 class Landmark(Place): pass
+
+class Event(Place):
+    date_and_time: datetime = Field(description="The date and time of the event")
+    price_options: List[float] = Field(description="A list of available prices, in EUR")
+
+class EventsReport(BaseModel):
+    report: List[Event] = Field(description="A list of notable events taking place at the time of the trip.")
 
 class LandmarksReport(BaseModel):
     report: List[Landmark] = Field(description="A list of the top landmarks for the destination")
@@ -43,10 +48,8 @@ class LandmarksReport(BaseModel):
 class EstablishmentReport(BaseModel):
     report: List[Establishment] = Field(description="A list for recommended places to go eat or drink.")
 
-class Event(Place):
-    date_and_time: datetime = Field(description="The date and time of the event")
-    price_options: List[float] = Field(description="A list of available prices, in EUR")
-
+class PlacesReport(BaseModel):
+    report: List[Place] = Field(description="A list of general purpose places to go.")
 
 class DestinationReport(BaseModel):
     landmarks: List[Landmark] = Field(description="A list of the top landmarks for the destination")
