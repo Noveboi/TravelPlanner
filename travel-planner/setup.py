1dfabb480d5449c1e637c12d88514a6dfe6bfc9b
George Nikolaidis <nove@novesoftware.net>
2025-09-21 19:30:35 +0300

    Use OpenRouter for dev LLM

--- travel-planner/setup.py
+++ travel-planner/setup.py
@@ -1,26 +1,33 @@
 ﻿import logging
 import os
 from datetime import date
+
+from langchain_openai import ChatOpenAI
+
 from agents.trip import TravelStyle, TripType, TripRequest
 from tools.tools import bind_necessary_tools
-from langchain_ollama import ChatOllama
+
+log = logging.getLogger('main')
+logging.basicConfig(level=logging.INFO)
 
 os.environ['TAVILY_API_KEY'] = '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'
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger('main')
+os.environ['OPENAI_API_KEY'] = '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'
 
 log.info('Starting')
-llm = ChatOllama(model='llama3.1:latest')
+llm = ChatOpenAI(
+    model='x-ai/grok-4-fast:free',
+    base_url="https://openrouter.ai/api/v1",
+)
 llm_with_tools = bind_necessary_tools(llm)
 log.info('Got LLM model')
 
 example_request = TripRequest(
     destination='London',
-    start_date=date(2025, 9, 16),
-    end_date=date(2025, 9, 21),
-    budget=2000,
+    start_date=date(2025, 12, 10),
+    end_date=date(2025, 12, 31),
+    budget=10000,
     travelers=2,
-    travel_styles=[TravelStyle.ADVENTURE],
+    travel_styles=[TravelStyle.CULTURAL],
     trip_type=TripType.COUPLE,
-    interests=['Sightseeing', 'Food']
+    interests=['Sightseeing', 'Local cuisine', 'Music of any type']
 )
