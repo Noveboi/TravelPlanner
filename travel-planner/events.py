7ae154e97471e601ce55ce8656ba69aa8d1bdb3e
George Nikolaidis <nove@novesoftware.net>
2025-09-21 19:32:53 +0300

    Add test scripts for events and establishments

---
+++ travel-planner/events.py
@@ -0,0 +1,8 @@
+﻿from agents.destination_scout import EventScoutAgent
+from setup import llm_with_tools, example_request
+
+if __name__ == '__main__':
+    agent = EventScoutAgent(llm_with_tools)
+    report = agent.invoke(example_request)
+
+    print(report.model_dump_json(indent=2))
